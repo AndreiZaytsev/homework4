a8f92b7f6788fa2e251f64f97955161ae865bcc7
Andry <strawer@mail.ru>
2022-08-09 13:38:11 +0300

    first commit

---
+++ src/test/java/guru/ga/PricingCompare.java
@@ -0,0 +1,29 @@
+package guru.ga;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class PricingCompare {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void pricing() {
+        open("https://github.com/");
+        $(byText("Pricing")).hover();
+        $(byText("Compare plans")).click();
+        $(".h2-mktg").shouldHave(text("Choose the plan that’s right for you."));
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+    }
+
+}
